f2667ca714ca740e70298cb800fd6bc21cdc805d
Paulo McNally <paulomcnally@gmail.com>
2019-03-12 11:21:41 -0600

    Add -am/-m flag

--- plugins/pt/pt.plugin.zsh
+++ plugins/pt/pt.plugin.zsh
@@ -1,7 +1,31 @@
 pt() {
-  $1 $2
+  $1 $2 $3
 }
 
+# Run git commit command.
+#
+# @argument $1
+#   -am
+#   -m
+#
+# @argument $2
+#   Text
 commit() {
-  git commit -m "$1" -m "[#`git rev-parse --abbrev-ref HEAD | awk -F'/' '{ print $2 }'`]"
+  [[ -z "$1" ]] && { error_message; return; }
+  [[ -z "$2" ]] && { error_message; return; }
+  if [[ $1 == "-am" ]] || [[ $1 == "-m" ]]; then
+    git commit $1 "$2" -m "[#`git rev-parse --abbrev-ref HEAD | awk -F'/' '{ print $2 }'`]"
+  else
+    error_message;
+    return;
+  fi
+}
+
+# show error message with example command
+function error_message() {
+  echo "The correct format is:";
+  echo ""
+  echo "   pt commit -m \"Commit message\"";
+  echo "or"
+  echo "   pt commit -am \"Commit message\"";
 }
